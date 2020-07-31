e29880cc2bbc91dafb6817b22698984c7fb1ee6f
dnov09 <dngige9@gmail.com>
2020-07-30 21:40:56 -0500

    task detail page complete
    - able to add efforts through detailed page

--- lib/Pages/taskDetailsPage.dart
+++ lib/Pages/taskDetailsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_habits/Pages/completedTaskPage.dart';
 import 'package:study_habits/constants/style.dart';
 import 'package:study_habits/Pages/taskcreator.dart';
 import 'package:study_habits/widgets/appBar.dart';
@@ -13,6 +14,19 @@ class TaskDetails extends StatefulWidget {
 }
 
 class TaskDetailsState extends State<TaskDetails> {
+  // Effort effort;
+  List<int> efforts = [];
+  var duration = 0;
+  final timeCtrl = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    timeCtrl.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: MyAppBar(), body: effortView());
@@ -48,12 +62,11 @@ class TaskDetailsState extends State<TaskDetails> {
             ),
           ),
           Divider(),
-          SizedBox(height: 8),
           Text.rich(
             TextSpan(
               children: <TextSpan>[
                 TextSpan(
-                    text: 'Due Date: \n',
+                    text: 'Due Date: \n\n',
                     style: TextStyle(fontFamily: 'Roboto', fontSize: 20)),
                 TextSpan(
                     text: widget.taskGotten.deadLine.toString(),
@@ -62,40 +75,139 @@ class TaskDetailsState extends State<TaskDetails> {
             ),
           ),
           Divider(),
-          SizedBox(height: 8),
           Text.rich(
             TextSpan(
-                text: 'Efforts Logged:',
-                style: TextStyle(fontFamily: 'Roboto', fontSize: 20)),
+              children: <TextSpan>[
+                TextSpan(
+                  text: 'Total time spent: ',
+                  style: TextStyle(fontFamily: 'Roboto', fontSize: 20),
+                ),
+                TextSpan(
+                  text: '$duration minutes',
+                  style: TextStyle(fontFamily: 'Roboto', fontSize: 20),
+                ),
+              ],
+            ),
           ),
-          SizedBox(height: 16),
-          SizedBox(
-            height: 48,
-            width: 160,
-            child: FlatButton(
-              color: Colors.green,
-              textColor: Colors.white,
-              disabledColor: Colors.grey,
-              disabledTextColor: Colors.black,
-              padding: EdgeInsets.all(8.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24),
-              ),
-              // splashColor: Colors.blueAccent,
-              onPressed: () {
-                //create new task
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          SizedBox(height: 8),
+          Text(
+            'Efforts Logged: ',
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.blueGrey,
+            ),
+          ),
+          SizedBox(height: 8),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: efforts.length,
+            itemBuilder: (context, idx) {
+              var date = new DateTime.now().toString();
+              var dateParse = DateTime.parse(date);
+              var currentDate =
+                  "${dateParse.month}/${dateParse.day}/${dateParse.year}";
+
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Icon(Icons.save),
                   Text(
-                    'Save',
-                    style: TextStyle(fontSize: 16),
+                    '$currentDate :',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(right: 16)),
+                  Text(
+                    '${efforts[idx].toString()} minutes',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.blueGrey,
+                    ),
                   )
                 ],
-              ),
+              );
+            },
+          ),
+          SizedBox(height: 24),
+          Form(
+            key: _formKey,
+            child: TextFormField(
+              controller: timeCtrl,
+              validator: (value) =>
+                  (value.isEmpty) ? "Please enter value" : null,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  hintText: 'How much time did you spend working today?'),
             ),
+          ),
+          SizedBox(height: 16),
+          Expanded(child: Container()),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 48,
+                width: 100,
+                child: FlatButton(
+                  color: Colors.blueGrey,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(8.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  // splashColor: Colors.blueAccent,
+                  onPressed: () {
+                    //create new task
+                    if (_formKey.currentState.validate()) {
+                      setState(() {
+                        duration += int.parse(timeCtrl.text);
+                        efforts.add(int.parse(timeCtrl.text));
+                        timeCtrl.clear();
+                      });
+                    }
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.save),
+                      Text(
+                        'Save',
+                        style: TextStyle(fontSize: 16),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48,
+                width: 160,
+                child: FlatButton(
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(8.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  // splashColor: Colors.blueAccent,
+                  onPressed: () {
+                    //create new task
+                    Navigator.popAndPushNamed(context, CompletedTaskPage.route);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.check),
+                      Text(
+                        'Complete Task',
+                        style: TextStyle(fontSize: 16),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
